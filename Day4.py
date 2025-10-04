0ae2919569907fff235b1391d7092e091c88b939
MdEhathisam <ehathisam67hotmail@gmail.com>
2025-10-05 00:20:30 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -13,8 +13,9 @@ question = st.text_input("Ask Anything")
 if st.button("Send"):
     response = myaibot.models.generate_content(
                model="gemini-2.5-flash",
-               contents = [myfile,question]
+               contents = ([myfile,question])
                )
 
     st.write(response.text)
+
 
