23400d96bd6ca72b755928d76f5c78509c645fb2
MdEhathisam <ehathisam67hotmail@gmail.com>
2025-10-04 12:45:16 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+from google import genai
+
+myaibot = genai.Client(api_key="AIzaSyB1YeNps3ZNWaLJpGF4TpHPSGXJq1EMr3c")
+
+st.title("My Own GPT")
+
+question = st.text_input("Ask Anything")
+
+if st.button("Send"):
+    response = myaibot.models.generate_content(
+               model="gemini-2.5-flash",
+               contents = question
+               )
+
+    st.write(response.text)
