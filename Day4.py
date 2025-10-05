24cbde377b5040fab37fd7833d559bb61ac6c0c0
MdEhathisam <ehathisam67hotmail@gmail.com>
2025-10-05 13:50:17 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -6,15 +6,44 @@ myaibot = genai.Client(api_key="AIzaSyB1YeNps3ZNWaLJpGF4TpHPSGXJq1EMr3c")
 
 st.title("My Own GPT")
 
-col1,col2 = st.columns([5, 1])
+st.markdown("""
+    <style>
+    .textarea-container {
+        position: relative;
+        width: 100%;
+    }
+    textarea {
+        width: 100% !important;
+        padding-right: 40px !important; /* space for button */
+    }
+    .plus-button {
+        position: absolute;
+        right: 10px;
+        bottom: 10px;
+        background-color: #0d6efd;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        width: 30px;
+        height: 30px;
+        cursor: pointer;
+    }
+    </style>
+    <div class="textarea-container">
+        <textarea id="custom-textarea" rows="4"></textarea>
+        <button class="plus-button" onclick="alert('Plus clicked!')">+</button>
+    </div>
+""", unsafe_allow_html=True)
 
-with col1:
-    question = st.text_area("Ask Anything", height = 10, label_visibility = "collapsed")
+#col1,col2 = st.columns([5, 1])
 
-with col2:
-    st.write("")
-    st.write("")
-    add_button = st.button("+",use_container_width = True)
+#with col1:
+ #   question = st.text_area("Ask Anything", height = 10, label_visibility = "collapsed")
+
+#with col2:
+ #   st.write("")
+  #  st.write("")
+   # add_button = st.button("+",use_container_width = True)
 
 send_button = st.button("Send")
 
@@ -29,6 +58,7 @@ if send_button:
                )
 
     st.write(response.text)
+
 
 
 
