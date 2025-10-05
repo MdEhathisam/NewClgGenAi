ffac47507976ac94e729f5c4e37c425f5f633ebe
MdEhathisam <ehathisam67hotmail@gmail.com>
2025-10-05 15:17:23 +0530

    Update FileUpload.py

--- FileUpload.py
+++ FileUpload.py
@@ -6,15 +6,22 @@ myaibot = genai.Client(api_key="AIzaSyB1YeNps3ZNWaLJpGF4TpHPSGXJq1EMr3c")
 
 st.title("My Own GPT")
 
-myfiles = st.file_uploader("Upload Image", type=["jpeg","jpg","png","pdf","doc","docx"])
+myfiles = st.file_uploader("Upload Files", type=["jpeg","jpg","png","pdf","doc","docx"])
 
 question = st.text_input("Ask Anything")
 
 if st.button("Send"):
   if myfiles is not None:
+    file_data = myfiles.read()
     response = myaibot.models.generate_content(
     model="gemini-2.5-flash",
-    contents = [{"role":"user","parts":[{"text":question},{"inline_data":{"mime_type":myfiles.type,"data":myfiles.read()}}]}]
+    contents = [
+      {"role":"user","parts":
+       [
+         {"text":question},
+         {"inline_data":{"mime_type":myfiles.type,"data":file_data}}
+       ]}
+    ]
     )
 
     st.write(response.text)
