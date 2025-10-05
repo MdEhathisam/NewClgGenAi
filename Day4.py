9ed1ce7638639f645cd2e033820113b7a2027a85
MdEhathisam <ehathisam67hotmail@gmail.com>
2025-10-05 14:28:47 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -1,5 +1,16 @@
+import streamlit as st
+
+from google import genai
+
+myaibot = genai.Client(api_key="AIzaSyB1YeNps3ZNWaLJpGF4TpHPSGXJq1EMr3c")
+
+st.title("My Own GPT")
 
 question = st.text_input("Ask Anything")
+
+with st.expander("Click to expand"):
+
+    st.write("This content is hidden by default and expanded by '+' icon.")
 
 if st.button("Send"):
 response = myaibot.models.generate_content(
