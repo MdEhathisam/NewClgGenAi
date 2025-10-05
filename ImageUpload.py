2af0adbe52a9acbeb700ff3fb17c8f6b636658f9
MdEhathisam <ehathisam67hotmail@gmail.com>
2025-10-05 15:02:23 +0530

    Update ImageUpload.py

--- ImageUpload.py
+++ ImageUpload.py
@@ -6,7 +6,7 @@ myaibot = genai.Client(api_key="AIzaSyB1YeNps3ZNWaLJpGF4TpHPSGXJq1EMr3c")
 
 st.title("My Own GPT")
 
-myimage = st.file_uploader("Upload Image", type=["jpeg","jpg","png"])
+myfiles = st.file_uploader("Upload Image", type=["jpeg","jpg","png","pdf","doc","docx"])
 
 question = st.text_input("Ask Anything")
 
@@ -14,12 +14,12 @@ if st.button("Send"):
   if myimage is not None:
     response = myaibot.models.generate_content(
     model="gemini-2.5-flash",
-    contents = [{"role":"user","parts":[{"text":question},{"inline_data":{"mime_type":myimage.type,"data":myimage.read()}}]}]
+    contents = [{"role":"user","parts":[{"text":question},{"inline_data":{"mime_type":myfiles.type,"data":myfiles.read()}}]}]
     )
 
     st.write(response.text)
   else:
     
-    st.write("Upload an image before sending")
+    st.write("Upload a file before sending")
 
 
