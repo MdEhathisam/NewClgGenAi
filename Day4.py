e0f6caa2a49048cc09a3b47fb73247a01ba3e168
MdEhathisam <ehathisam67hotmail@gmail.com>
2025-10-05 14:59:12 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -6,21 +6,16 @@ myaibot = genai.Client(api_key="AIzaSyB1YeNps3ZNWaLJpGF4TpHPSGXJq1EMr3c")
 
 st.title("My Own GPT")
 
-myimage = st.file_uploader("Upload Image", type=["jpeg","jpg","png"])
-
 question = st.text_input("Ask Anything")
 
+with st.expander("Click to expand"):
+
+    st.write("This content is hidden by default and expanded by '+' icon.")
+
 if st.button("Send"):
-  if myimage is not None:
-    response = myaibot.models.generate_content(
-    model="gemini-2.5-flash",
-    contents = [{"role":"user","parts":[{"text":question},{"inline_data":{"mime_type":myimage.type,"data":myimage.read()}}]}]
-    )
+  response = myaibot.models.generate_content(
+  model="gemini-2.5-flash",
+  contents = question
+  )
 
-    st.write(response.text)
-  else:
-    
-    st.write("Upload an image before sending")
-
-
-
+  st.write(response.text)
