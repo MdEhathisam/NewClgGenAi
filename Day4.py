0730a1cca985001a82ff3b346a3048cd6399f612
MdEhathisam <ehathisam67hotmail@gmail.com>
2025-10-05 14:59:50 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -8,10 +8,6 @@ st.title("My Own GPT")
 
 question = st.text_input("Ask Anything")
 
-with st.expander("Click to expand"):
-
-    st.write("This content is hidden by default and expanded by '+' icon.")
-
 if st.button("Send"):
   response = myaibot.models.generate_content(
   model="gemini-2.5-flash",
@@ -19,3 +15,4 @@ if st.button("Send"):
   )
 
   st.write(response.text)
+
