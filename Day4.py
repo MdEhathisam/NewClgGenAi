672976d010ca644ba5566e7bf93620d012907e5a
MdEhathisam <ehathisam67hotmail@gmail.com>
2025-11-20 22:02:33 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -5,53 +5,15 @@ from google import genai
 myaibot = genai.Client(api_key="AIzaSyB1YeNps3ZNWaLJpGF4TpHPSGXJq1EMr3c")
 
 st.title("My Own GPT")
-
-#Chat History Setup
-
-if "messages" not in st.session_state:
-
-    st.session_state.messages = []   
-# Display chat history
-
-for msg in st.session_state.messages:
-
-    if msg["role"] == "user":
-
-        st.chat_message("user").markdown(msg["content"])
-
-    else:
-
-        st.chat_message("assistant").markdown(msg["content"])
-
+if 
 
 question = st.text_input("Ask Anything")
 
-if question:
+if st.button("Send"):
+  response = myaibot.models.generate_content(
+  model="gemini-2.5-flash",
+  contents = question
+  )
 
-    # Save user message
-
-    st.session_state.messages.append({"role": "user", "content": question})
-
-    st.chat_message("user").markdown(question)
-
-    # Send request to AI model
-
-    response = myaibot.models.generate_content(
-
-        model="gemini-2.5-flash",
-
-        contents=question
-
-    )
-
-    ai_reply = response.text
-
-    # Save bot message
-
-    st.session_state.messages.append({"role": "assistant", "content": ai_reply})
-
-    st.chat_message("assistant").markdown(ai_reply)
-
-
-
-
+  st.write(response.text)
+    
