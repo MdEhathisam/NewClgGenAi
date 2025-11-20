7a2c36b08c47f9878929fda9d1956d21cbcb8075
MdEhathisam <ehathisam67hotmail@gmail.com>
2025-11-20 21:23:50 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -1,25 +1,18 @@
 import streamlit as st
+
 from google import genai
 
-# Setup Gemini client (replace api_key with your key)
-myaibot = genai.Client(api_key="YOUR_API_KEY")
+myaibot = genai.Client(api_key="AIzaSyB1YeNps3ZNWaLJpGF4TpHPSGXJq1EMr3c")
 
 st.title("My Own GPT")
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
 question = st.text_input("Ask Anything")
 
-if st.button("Send") and question:
-    st.session_state.messages.append({"role": "user", "content": question})
-    response = myaibot.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=st.session_state.messages
-    )
-    st.session_state.messages.append({"role": "assistant", "content": response.text})
+if st.button("Send"):
+  response = myaibot.models.generate_content(
+  model="gemini-2.5-flash",
+  model="gemini-1.5-flash",
+  contents = question
+  )
 
-for msg in st.session_state.messages:
-    with st.chat_message(msg["role"]):
-        st.markdown(msg["content"])
-
+  st.write(response.text)
