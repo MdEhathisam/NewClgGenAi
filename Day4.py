75389fdec58a8383b747ec1b3f46eee6fc41be36
MdEhathisam <ehathisam67hotmail@gmail.com>
2025-11-20 21:08:36 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -12,17 +12,18 @@ question = st.text_input("Ask Anything")
 
 if st.button("Send") and question:
        st.session_state.messages.append({"role":"user","content": question})
-     response = 
-myaibot.models.generate_content(
+       
+       response = myaibot.models.generate_content(
+       
+       model="gemini-2.5-flash",
   
-  model="gemini-2.5-flash",
-  
-  contents = st.session_state.messages
+       contents = st.session_state.messages
   )
 st.session_state.messages.append({"role":"assistant","content": response.text})
 for msg in st.session_state.messages:
      with
 st.chat_message(msg["role"]):
 st.markdown(msg["content"])
+
 
   #st.write(response.text)
